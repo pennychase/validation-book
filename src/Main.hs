dcd73ee835961692fd96e1ff3abce54860696cf3
Penny Chase <mpchase@mac.com>
2020-01-12 21:46:23 -0500

    Chapter 8 - Refactored to use Validation to capture multiple errors. This required modifying the cabal file to include a dependency on validate.

--- src/Main.hs
+++ src/Main.hs
@@ -1,123 +1,111 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
 
 import Data.Char
+import Data.Validation
 
 -------------------------------------------------------------------------------
--- Chapter 7
--- Created User datatype and the function makeUser
--- Modified main to prompt for a username and password, and then construct
--- the user from these arguments (after validating). main uses fmap (<$>) and
--- applicative (<*>) to collect these independent inputs and pass them to
--- makeUser
+-- Chapter 8
+-- Refactored to use Validate
+-- Changed Error to be a list of Strings in order to capture all the
+-- possible errors
 -------------------------------------------------------------------------------
 
+-- Types
+
+-- Password
 newtype Password = Password String
   deriving (Show, Eq)
 
-newtype Error = Error String
-  deriving (Show, Eq)
-
+-- Username
 newtype Username = Username String
   deriving (Show, Eq)
 
+-- User
 data User = User Username Password
   deriving (Show, Eq)
 
+-- Error
+-- Error needs a Semigroup instance, and since it's really the same as the
+-- instance for List, we can use derving to create the instance
+newtype Error = Error [String]
+  deriving (Show, Eq, Semigroup)
+
 -- Check that the password length is within the lower and upper bounds
-checkPasswordLength :: String -> Either Error Password
+checkPasswordLength :: String -> Validation Error Password
 checkPasswordLength password =
   case (length password > 20) of
-    True -> Left (Error "Your password cannot be longer than 20 characters")
-    False -> Right (Password password)
+    True -> Failure (Error ["Your password cannot be longer than 20 characters"])
+    False -> Success (Password password)
 
 -- Check that the username length is within bounds
-checkUsernameLength :: String -> Either Error Username
+checkUsernameLength :: String -> Validation Error Username
 checkUsernameLength name =
   case (length name > 15) of
-    True -> Left (Error "Your username cannot be longer than 15 characters")
-    False -> Right (Username name)
+    True -> Failure (Error ["Your username cannot be longer than 15 characters"])
+    False -> Success (Username name)
 
 -- Refactor checking username and password length
-checkLength :: Int -> String -> Either Error String
+checkLength :: Int -> String -> Validation Error String
 checkLength n str =
   case (length str > n) of
-    True -> Left (Error errMsg)
-    False -> Right str
+    True -> Failure (Error errMsg)
+    False -> Success str
   where
-    errMsg = "Cannot be longer than " ++ show n ++ " characters"
+    errMsg = ["Cannot be longer than " ++ show n ++ " characters"]
 
 -- Check that the string is composed only of alphnumeric characters
-requiredAlphaNum :: String -> Either Error String
-requiredAlphaNum xs =
+requireAlphaNum :: String -> Validation Error String
+requireAlphaNum xs =
   case (all isAlphaNum xs) of
-    False -> Left (Error "Cannot contain white space or \
-                  \special characters")
-    True -> Right xs
+    False -> Failure (Error ["Cannot contain white space or \
+                  \special characters"])
+    True -> Success xs
 
 -- Remove leading white space and reject empty strings
-cleanWhiteSpace :: String -> Either Error String
-cleanWhiteSpace "" = Left (Error "Cannot be empty")
+cleanWhiteSpace :: String -> Validation Error String
+cleanWhiteSpace "" = Failure (Error ["Cannot be empty"])
 cleanWhiteSpace (x:xs) =
   case (isSpace x) of
     True -> cleanWhiteSpace xs
-    False -> Right (x:xs)
+    False -> Success (x:xs)
 
 -- validatePassword first strips leading white space from the input password,
 -- possibly returing a transformed string. It then checks the other
--- password requirements. validatePassword returns an EIther Error Password, so
--- the last validation must return Either Error Password (so at this point
+-- password requirements. validatePassword returns an Validation Error Password, so
+-- the last validation must return Validation Error Password (so at this point
 -- the order matters)
-validatePassword :: Password -> Either Error Password
+validatePassword :: Password -> Validation Error Password
 validatePassword (Password pwd) =
-  cleanWhiteSpace pwd
-    >>= requiredAlphaNum
-    >>= checkPasswordLength
-
--- Version using checkLength
-validatePassword' :: Password -> Either Error Password
-validatePassword' (Password pwd) =
-  Password <$>
-    (cleanWhiteSpace pwd
-    >>= requiredAlphaNum
-    >>= checkLength 20)
-
--- Version using do notation
-validatePassword'' :: Password -> Either Error Password
-validatePassword'' (Password pwd) =
-  do
-    pwd' <- cleanWhiteSpace pwd
-    pwd'' <- requiredAlphaNum pwd'
-    checkPasswordLength pwd''
+  case (cleanWhiteSpace pwd) of
+    Failure err -> Failure err
+    Success pwd' -> requireAlphaNum pwd' *>
+                    checkPasswordLength pwd'
 
 -- validateUsername validates usernames, first stripping white space and then
 -- checking the otehr rwquirements. checkUsernameLength is the last function
 -- applied because it rturns a Username when successdul
-validateUsername :: Username -> Either Error Username
+validateUsername :: Username -> Validation Error Username
 validateUsername (Username name) =
-  cleanWhiteSpace name
-    >>= requiredAlphaNum
-    >>= checkUsernameLength
-
--- Version using checkLength
-validateUsername' :: Username -> Either Error Username
-validateUsername' (Username name) =
-  Username <$>
-    (cleanWhiteSpace name
-      >>= requiredAlphaNum
-      >>= checkLength 15)
+  case (cleanWhiteSpace name) of
+    Failure err -> Failure err
+    Success name' -> requireAlphaNum name' *>
+                     checkUsernameLength name'
 
 -- Constructing a User
-makeUser :: Username -> Password -> Either Error User
+makeUser :: Username -> Password -> Validation Error User
 makeUser name pwd =
   User <$> validateUsername name
        <*> validatePassword pwd
 
 -- Construct a User with a default temporary password
-makeUserTmpPassword :: Username -> Either Error User
+makeUserTmpPassword :: Username -> Validation Error User
 makeUserTmpPassword name =
   User <$> validateUsername name
        <*> pure (Password "tempPassword")
 
+{--
 -- Testing
 -- Test results are represented as Either String ()
 -- Left is the string describing the failure and Right does nothing so is ()
@@ -149,10 +137,11 @@ test = printTestResult $
                 \between 10 and 20 characters"))
     eq 2 (checkPasswordLength (replicate 25 'a')) (Left (Error "Your password must be \
                 \between 10 and 20 characters"))
-    eq 3 (requiredAlphaNum "hellothere123") (Right "hellothere123")
-    eq 4 (requiredAlphaNum "hellothere!!!") (Left (Error "Cannot contain white \
+    eq 3 (requireAlphaNum "hellothere123") (Right "hellothere123")
+    eq 4 (requireAlphaNum "hellothere!!!") (Left (Error "Cannot contain white \
                   \space or special characters"))
     eq 5 (checkPasswordLength "") (Right (Password "\"\""))-- To demonstrate failed test
+--}
 
 -- main using bind
 main' :: IO ()
